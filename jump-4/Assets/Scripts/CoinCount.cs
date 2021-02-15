009caa0769b0d12866ecd3d03cd09fc08873e9a4
theamayen <72680199+theamayen@users.noreply.github.com>
2021-02-16 00:20:59 +0800

    coin manager

--- jump-4/Assets/Scripts/CoinCount.cs
+++ jump-4/Assets/Scripts/CoinCount.cs
@@ -5,23 +5,27 @@ using UnityEngine.UI;
 
 public class CoinCount : MonoBehaviour
 {
-    private int coinCount;
+    private int coinCount = 0;
+    private int coinCount2 = 0;
 
     public Text coin;
 
     void Start()
     {
-        if(PlayerPrefs.HasKey("coin"))
+        if (PlayerPrefs.HasKey("coin"))
         {
-            coinCount = PlayerPrefs.GetInt("coin");
-            coin.text = coinCount.ToString();
+            coinCount2 = PlayerPrefs.GetInt("coin");
+            coin.text = coinCount2.ToString();
         }
     }
 
     // Update is called once per frame
     public void CoinCounter()
     {
-        coinCount++;
+        coinCount2++;
+        PlayerPrefs.SetInt("coin", coinCount2);
+        coinCount = PlayerPrefs.GetInt("coin");
         coin.text = coinCount.ToString();
+
     }
 }
