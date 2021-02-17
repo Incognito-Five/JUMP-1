d300156b93303fe636f10ef1463f775a697d4585
taemagura <72679657+taemagura@users.noreply.github.com>
2021-02-17 15:27:32 +0800

    Game Over Screen

---
+++ jump-4/Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameOverScreen GameOverScreen;
+    int maxPlatform = 0;
+
+    public void GameOver()
+    {
+        GameOverScreen.Setup(maxPlatform);
+    }
+
+}
