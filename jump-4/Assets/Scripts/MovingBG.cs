fb7908ff0819bd5e45f2ab98b18522477023f4fc
theamayen <72680199+theamayen@users.noreply.github.com>
2021-02-17 13:06:38 +0800

    movingBG

---
+++ jump-4/Assets/Scripts/MovingBG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBG : MonoBehaviour
+{
+    public float speed;
+    public float clampPos;
+    [HideInInspector] public Vector3 StartPos;
+
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * speed, clampPos);
+        transform.position = StartPos + Vector3.left * newPos;
+    }
+}
